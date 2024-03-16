2f99e76f8bd84ca19ba3e1f83ed12146e71f6e41
1NJ57C3 <85198277+1NJ57C3@users.noreply.github.com>
2024-03-15 21:10:25 -0700

    Added error handling/validation and some more "flavor"

--- 04-higher-lower-guessing-game.py
+++ 04-higher-lower-guessing-game.py
@@ -17,7 +17,7 @@ def generate_number():
   return random.randint(1, 100)
 
 def gameloop(prompt, num, score=0):
-  response = input(prompt + "\n> ")
+  response = input(prompt + "\n> ").lower()
 
   def failure(direction):
     if score < 5:
@@ -27,20 +27,25 @@ def gameloop(prompt, num, score=0):
     else:
       return flavor[random.randrange(3, len(flavor))] + direction.upper()
 
-  if "quit" in response or "exit" in response:
-    print("Fine. I've got better things to do, anyway!")
-  elif int(response) < num:
-    gameloop(failure("higher"), num, score+1)
-  elif int(response) > num:
-    gameloop(failure("lower"), num, score+1)
-  elif str(num) in response:
-    print("Congratulations! You win and get... Nothing!")
-    print("Guesses: " + str(score))
-    response = input("Would you like to play again? (y/n)\n")
-    if "y" in response.lower():
-      gameloop("Here we go, again. 1 to 100. Hit me.", generate_number())
+  try:
+    int(response)
+    if int(response) < num:
+      gameloop(failure("higher"), num, score+1)
+    elif int(response) > num:
+      gameloop(failure("lower"), num, score+1)
+    elif str(num) in response:
+      print("Congratulations! You win and get... Nothing!")
+      print("Guesses: " + str(score))
+      response = input("Would you like to play again? (y/n)\n")
+      if "y" in response.lower():
+        gameloop("Here we go, again. 1 to 100. Hit me.", generate_number())
+      else:
+        print("Goodbye")
+  except ValueError:
+    if any(word in response for word in ["quit", "exit", "give up", "giving up"]) or response == "q":
+      print(exit_messages[random.randrange(len(exit_messages))])
     else:
-      print("Goodbye")
+      gameloop(failed_validation[random.randrange(len(failed_validation))], num, score)
 
 flavor = [
   "Aim ",
@@ -48,6 +53,21 @@ flavor = [
   "Maybe ",
   "No. ",
   "Dude. ",
+  "Bruh. ",
+  "Come on! ",
+]
+
+failed_validation = [
+  "Unless you're quitting, numbers only, please.",
+  "You're supposed to give me a number.",
+  "This is a NUMBER guessing game, idiot!",
+]
+
+exit_messages = [
+  "Fine. I've got better things to do, anyway!",
+  "Whatever.",
+  "Okay, bye.",
+  "Ugh.",
 ]
 
 gameloop("I am thinking of a number between 1 and 100. Guess what it is. If you guess wrong, I'll give you a hint.", generate_number())
